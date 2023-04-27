a04d6b6b582bdacd842e6d80b23aeb340c21529b
Cooper Vandiver <rvandiver@una.edu>
2023-04-27 18:56:16 -0500

    Fixed .gitignore and pruned

--- cpp/build/getDetTime.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-
-times = []
-for i in range(100):
-	result = subprocess.run(["./target"], stdout=subprocess.PIPE, check=True, text=True)
-	times.append(int(result.stdout.split('\n')[0].split()[2]))
-avg = sum(times)/len(times)
-print(f"C++ Determinant Avg: {avg/1000} ms")
